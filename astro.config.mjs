f9b2ce5c3ea403d08e711ce8ce67af666d9d386c
andrre-ls <43184224+andrre-ls@users.noreply.github.com>
2025-11-30 14:17:49 +0000

    fix: base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 
 import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
+import { PATH_BASE } from "./src/constants";
 
 export default defineConfig({
-  integrations: [mdx()],
-  server: {
-    port: 3000,
-  },
-  devToolbar: {
-    enabled: false,
-  },
+	integrations: [mdx()],
+	base: PATH_BASE,
+	server: {
+		port: 3000,
+	},
+	devToolbar: {
+		enabled: false,
+	},
 });
